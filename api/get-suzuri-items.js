f3261d140f2e1c65f4f409a886ef4fe21a08d53c
syuttyoseibi <daishi2matsuda@gmail.com>
2025-09-06 12:49:59 +0900

    Suzuri追加２

--- api/get-suzuri-items.js
+++ api/get-suzuri-items.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   }
 
   try {
-    const response = await fetch(`https://suzuri.jp/api/v1/user/${suzuriUserId}/items`, {
+    const response = await fetch(`https://suzuri.jp/api/v1/users/${suzuriUserId}/items`, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${suzuriApiKey}`
